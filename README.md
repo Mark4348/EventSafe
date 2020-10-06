3176dc12d2ecbba1a98219fd11dbf0c5e8849ec2
MarkA <m_aguilar_2011@yahoo.com>
2020-10-06 10:52:06 -0500

    added intitial api urls

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # EventSafe
+An app for the cautious event-goer to view covid statistics of 
+
+## User Story
+```
+
+
+##Acceptance Criteria
---
+++ scripts.js
@@ -0,0 +1,7 @@
+$(document).ready() {
+
+var covidUrl = "https://disease.sh/v3/covid-19/jhucsse/counties/" + $("#userInput")
+var ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey="
+var apiKey = "vY3AfxyObNc8DzjJkrHSMIJeaYGpaJp5"
+
+}
