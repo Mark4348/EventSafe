568a99c61584188f7025554f216a7b46c39474de
MarkA <m_aguilar_2011@yahoo.com>
2020-10-07 11:48:15 -0500

    apis not ready

--- scripts.js
+++ scripts.js
@@ -5,28 +5,41 @@ var ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events.json?api
 var apiKey = "vY3AfxyObNc8DzjJkrHSMIJeaYGpaJp5"
 
 
+    //city search will give us event info, county name, and lat and long
 
-    // var covidUrl = "https://disease.sh/v3/covid-19/jhucsse/counties/";
-    // var ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=";
-    // var apiKey = "vY3AfxyObNc8DzjJkrHSMIJeaYGpaJp5";
+    //with that info will grab lat and long
+
+    //feed that into location api
+
+    //which will give us county name then connect that to api
+
+
 
 
     //Covid API
+
     $.ajax({
-        url: "https://disease.sh/v3/covid-19/jhucsse/counties/",
+        url: "https://disease.sh/v3/covid-19/states/" + $("#userState"),
         method: "GET"
     })
     .then(function(data) {
         console.log(data)
+        $("").text(data)
+        $("").text(data)
+        //grab death rate
+        //grab positive
     })
     
     //Events API
     $.ajax({
-        url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=vY3AfxyObNc8DzjJkrHSMIJeaYGpaJp5",
+        url: "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=sport&city=" + $("#userCity") + "&apikey=vY3AfxyObNc8DzjJkrHSMIJeaYGpaJp5",
         method: "GET"
     })
     .then(function(data) {
         console.log(data)
+        //grab events classification
+        //grab user zipcode
+        //grab
     })
 
 
