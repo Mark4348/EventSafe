f1fce471c4d4b0400140d048026b3a22f660509a
Elliott Payne <e.payne000@gmail.com>
2020-10-07 22:43:09 -0500

    cleaned up js, removed an ajax call that was no longer needed.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     var history = JSON.parse(localStorage.getItem("city")) || [];
     var city;
 
-
     $("#searchBtn").on("click", function () {
         city = $("#searchInput").val();
         history.unshift(city);
@@ -11,13 +10,7 @@ $(document).ready(function () {
         getEvents(city)
     })
 
-
-
     var apiKey = "vY3AfxyObNc8DzjJkrHSMIJeaYGpaJp5"
-    //var covidUrl = "https://disease.sh/v3/covid-19/jhucsse/counties/" + county;
-    //var ticketMasterUrl = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=sport&city=" + city + "&apikey=" + apiKey;
-    //var geoUrl = "https://geo.fcc.gov/api/census/block/find?latitude=" + lat + "&longitude=" + lon + "&showall=false&format=json"
-
 
     function getEvents(city) {
 
@@ -34,25 +27,23 @@ $(document).ready(function () {
 
             var test = info;
             console.log(info)
-            var lat = info._embedded.events[0]._embedded.venues[0].location.latitude;
-            var lon = info._embedded.events[0]._embedded.venues[0].location.longitude;
+            var state = info._embedded.events[0]._embedded.venues[0].state.name;
+            console.log(state);
 
             var list = info._embedded.events;
             
             for (var i = 0; i < list.length; i++) {
-                console.log(i);
-                console.log(test._embedded.events);
 
                 var event = test._embedded.events[i].name;
-                console.log(test._embedded.events);
+                
                 var date = test._embedded.events[i].dates.start.localDate;
-                console.log(date);
+                
                 var location = test._embedded.events[i]._embedded.venues[0].name;
-                console.log(location);
+                
                 var address = test._embedded.events[i]._embedded.venues[0].address.line1;
-                console.log(address);
+                
                 var links = test._embedded.events[i].url;
-                console.log(links);
+                
                 var image = test._embedded.events[i].images[0].url
     
                 var info = $(`
@@ -75,51 +66,29 @@ $(document).ready(function () {
                         </div>
                     </div>
                `)
-                console.log(info);
+                
     
                 $("#eventSection").append(info)
             }
-            getLoc(lat, lon);
+            getCovid(state);
             
         })
     }
 
-    function getLoc (lat, lon){
-        
-        $.ajax({
-            url: "https://geo.fcc.gov/api/census/block/find?latitude=" + lat + "&longitude=" + lon + "&showall=false&format=json",
-            method: "GET"
-        }).then(function (a) {
-            console.log(a)
-            var county = a.County.name;
-            getCovid(county);
-        })
+    
 
-    }
-
-    function getCovid (county){
+    function getCovid (state){
         //Covid API
         $.ajax({
-            url: "https://disease.sh/v3/covid-19/jhucsse/counties/" + county,
+            url: "https://disease.sh/v3/covid-19/states/" + state + "?yesterday=true",
             method: "GET"
         }).then(function (data) {
             console.log(data)
+            //var test1 = data;
+            //var countyEv = test1.County.name;
+            //console.log(countyEv)
         })
     }
     
 })
-
-
-
-
-                    //<li class="collection-item avatar">
-                    //<img src="#" alt="" class="circle">
-                    //<span class="title" >${event}</span> 
-                    //<hr>
-                    //<p>${date}</p> <hr>
-                    //<p>${location}</p> <hr>
-                    //<p>${address}</p> 
-                    //<a href="${links}" class="secondary-content"><i class="material-icons">grade</i></a>
-                    //</li>
-                    //<hr>
-                    //<br></br>
+        
